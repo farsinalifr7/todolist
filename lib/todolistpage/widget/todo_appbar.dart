df98308e8667d6aa45e5e8bca6d637c712e6f4fc
farsinalifr7 <farsinalifr7@gmail.com>
2024-02-26 10:47:08 +0530

    checking

--- lib/todolistpage/widget/todo_appbar.dart
+++ lib/todolistpage/widget/todo_appbar.dart
@@ -5,7 +5,7 @@ AppBar buildAppBar() {
       elevation: 0,
       centerTitle: true,
       backgroundColor: Colors.grey[200],
-      title: const Text("TO DO LIST"),
+      title: const Text("TO DO LIST "),
       titleTextStyle: const TextStyle(
           fontSize: 40,
           letterSpacing: 7,
